8aee2bc3b4b3daa6d8f84dd274c4035eeed4b77d
Terumi <terumi@PC2410.local>
2025-08-22 12:04:42 +0900

    Capico Admin icon

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -28,7 +28,7 @@
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
 	<meta name="apple-mobile-web-app-title" content="Capico Admin" />
-	<link rel="apple-touch-icon" sizes="180x180" href="/Capico.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/CapicoAdmin.png" />
 </svelte:head>
 
 <main>
